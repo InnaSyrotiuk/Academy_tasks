c074f4b7510dc176fcb34ac43bcde3f2bb2ce9a3
Inna <inna.syrotiuk@gmail.com>
2018-06-15 14:52:49 +0300

    DataImporter

---
+++ DataImporter/DataImporter/DataReader.cs
@@ -0,0 +1,23 @@
+﻿using DataImporter.DataAccess;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImporter
+{
+    public class DataReader
+    {
+        public List<News> Read(string path)
+        {
+            List<News> result = new List<News>();
+
+            string text = File.ReadAllText(path);
+            result = JsonConvert.DeserializeObject<List<News>>(text);
+            return result;
+        }
+    }
+}
---
+++ DataImporter/DataImporter/Program.cs
@@ -0,0 +1,55 @@
+﻿using DataImporter.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataReader reader = new DataReader();
+            List<News> data = reader.Read("News.json");
+                //AddComments();
+                //UpdateComment();
+            }
+
+        public static void AddComments()
+        {
+            using(OnlineExamDB db = new OnlineExamDB())
+            {
+
+                Comment comment = new Comment();
+                comment.CommentText = "Hello from Inna";
+                comment.CreationDateTime = DateTime.Now;
+                comment.ExerciseId = 2;
+                comment.Rating = 50;
+                comment.UserId = "03c8563b-3495-4691-8e4b-0db1d1767223";
+                comment.UserName = "Test user name";
+
+                db.Comments.Add(comment);
+
+                
+                db.SaveChanges();
+
+            }
+        }
+
+        public static void UpdateComment()
+        {
+            using (OnlineExamDB db = new OnlineExamDB())
+            {
+                Comment existingComment = db.Comments.FirstOrDefault(com => com.CommentText == "Hello from Inna");
+
+                if(existingComment != null)
+                {
+                    existingComment.Rating = 3;
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
